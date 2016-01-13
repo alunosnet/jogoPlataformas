7b9a128a9a734466ea8530f9608af4a4bb62928a
alunosnet <alunosnet@gmail.com>
2016-01-13 17:50:31 +0000

    Inicio

---
+++ jogoPlataformas/EstruturaJogo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace jogoPlataformas
+{
+    //Classe que implementa:
+    // - Menu do jogo
+    // - ciclo do Jogo
+    class EstruturaJogo
+    {
+        KeyboardState teclado;
+        GamePadState gamepad;
+        Game game;
+        int estado; //0-Menu; 1-Jogo; 2-Pausa
+        public EstruturaJogo(Game game)
+        {
+            this.game = game;
+        }
+        public void Initialize()
+        {
+            estado = 0;
+        }
+        public void LoadContent()
+        {
+
+        }
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
