0f915e9bc940ad9fb681f903b30bcdbcdc43c06a
alunosnet <alunosnet@gmail.com>
2016-01-13 18:44:41 +0000

    Desenhar e atualizar menu

--- jogoPlataformas/EstruturaJogo.cs
+++ jogoPlataformas/EstruturaJogo.cs
@@ -17,8 +17,10 @@ namespace jogoPlataformas
         GamePadState gamepad;
         Game game;
         int estado; //0-Menu; 1-Jogo; 2-Pausa
+        int opMenu; //posição selecionada no menu
         Texture2D opJogar;
         Texture2D opSair;
+        bool disparou = false;
 
         public EstruturaJogo(Game game)
         {
@@ -27,6 +29,8 @@ namespace jogoPlataformas
         public void Initialize()
         {
             estado = 0;
+
+            opMenu = 1;
         }
         public void LoadContent()
         {
@@ -36,8 +40,53 @@ namespace jogoPlataformas
         public void Update(GameTime gameTime)
         {
             //atualizar jogo
-            
+            if (estado == 0) atualizarMenu();
             //atualizar menu
+
+        }
+        public void atualizarMenu()
+        {
+            
+            //teclas
+            teclado = Keyboard.GetState();
+
+            gamepad = GamePad.GetState(PlayerIndex.One);
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                game.Exit();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed)
+                return;
+
+
+            if (teclado[Keys.Down] == KeyState.Down || teclado[Keys.Up] == KeyState.Down || teclado[Keys.Enter] == KeyState.Down)
+            {
+                if (disparou == true) return;
+                disparou = true;
+            }
+            else
+                disparou = false;
+
+            if (teclado[Keys.Up] == KeyState.Down)
+            {
+                disparou = true;
+                opMenu--;
+            }
+            if (teclado[Keys.Down] == KeyState.Down)
+            {
+                opMenu++;
+                disparou = true;
+            }
+            if (opMenu < 1) opMenu = 2;
+            if (opMenu > 2) opMenu = 1;
+
+            if (teclado[Keys.Enter] == KeyState.Down)
+            {
+                if (opMenu == 1) estado = 1;
+                if (opMenu == 2) game.Exit();
+            }
+
+            return;
 
         }
         public void Draw(GameTime gameTime,GraphicsDevice dispositivo, SpriteBatch spriteBatch)
@@ -62,13 +111,14 @@ namespace jogoPlataformas
             Color cor;
 
             
-            if (estado==0)
+            if (opMenu==1)
                 cor = Color.White;
             else
                 cor = Color.Brown;
             rtemp = new Rectangle(0, 0, opJogar.Width, opJogar.Height);
             spriteBatch.Draw(opJogar, rtemp, cor);
-            if (estado==1)
+
+            if (opMenu==2)
                 cor = Color.White;
             else
                 cor = Color.Brown;
@@ -81,5 +131,6 @@ namespace jogoPlataformas
         public void desenhaJogo(GameTime gameTime, SpriteBatch spriteBatch)
         {
         }
-        }
+
+    }
 }
